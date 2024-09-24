325c0d06a68cf3d235648fa002be04d49bd0404f
PuppetRuler <ziyelixiang@gmail.com>
2024-09-24 12:51:58 +0800

    fix

--- docs/public/js/rightmenu.js
+++ docs/public/js/rightmenu.js
@@ -172,14 +172,13 @@ const images = [
   'url("https://fastly.jsdelivr.net/gh/PuppetRuler/drawing-board@main/images/1724780835507large_evb08_a.jpg")',
   'url("https://fastly.jsdelivr.net/gh/PuppetRuler/drawing-board@main/images/1724787392341%E3%81%9D%E3%81%AE%E4%BB%96_%E3%83%A1%E3%82%A4%E3%83%B3%E3%83%93%E3%82%B8%E3%83%A5%E3%82%A2%E3%83%AB%E8%A3%8F_01.jpg")',
   'url("https://fastly.jsdelivr.net/gh/PuppetRuler/drawing-board@main/images/1724787326109%E3%81%9D%E3%81%AE%E4%BB%96_%E3%82%AD%E3%83%A3%E3%83%A9%E7%B4%B9%E4%BB%8B%EF%BC%9A%E3%83%9F%E3%83%AA%E3%83%A3_01.jpg")',
-  'url("https://fastly.jsdelivr.net/gh/PuppetRuler/drawing-board@main/images/1727145787406bg_p0.jpg")',
   'url("https://fastly.jsdelivr.net/gh/PuppetRuler/drawing-board@main/images/1724787637521%E3%82%A2%E3%82%A4_%E5%A4%A2%E3%81%AE%E7%B5%82%E3%82%8F%E3%82%8A%E3%81%AB%EF%BC%88%E6%B5%81%E8%8A%B1%E3%83%BB%E3%82%82%E3%82%82%EF%BC%89_01.jpg")',
   'url("https://fastly.jsdelivr.net/gh/PuppetRuler/drawing-board@main/images/1724780599506large_eva04_a.jpg")',
   'url("https://fastly.jsdelivr.net/gh/PuppetRuler/drawing-board@main/images/1724778936500large_Ren_01d.png")',
   'url("https://fastly.jsdelivr.net/gh/PuppetRuler/drawing-board@main/images/1724779116501large_Syugou_01a.png")',
   'url("https://fastly.jsdelivr.net/gh/PuppetRuler/drawing-board@main/images/1724778729498large_Haru_03b.png")',
   'url("https://fastly.jsdelivr.net/gh/PuppetRuler/drawing-board@main/images/1724780878506large_evc05_a.jpg")',
-  'url("https://fastly.jsdelivr.net/gh/PuppetRuler/drawing-board@main/images/1726620907142bg.jpg")'
+  'url("https://fastly.jsdelivr.net/gh/PuppetRuler/drawing-board@main/images/1727145787406bg_p0.jpg")'
 ];
 
 let currentIndex = 0;
