1b5b0bb307140298a2ad5a517a1b9c53b8b78067
PuppetRuler <ziyelixiang@gmail.com>
2024-09-25 12:40:26 +0800

    fix

--- docs/public/js/oml2d-canvas.js
+++ docs/public/js/oml2d-canvas.js
@@ -3,10 +3,12 @@ let homePage;
 const observer = new MutationObserver(() => {
   stage = document.querySelector("#oml2d-stage");
   homePage = document.querySelector(".VPHome");
-  if (!homePage) {
-    stage.style.zIndex = '-10'
-  } else {
-    stage.style.zIndex = '9998';
+  if(stage){
+    if (!homePage) {
+      stage.style.zIndex = '-10'
+    } else {
+      stage.style.zIndex = '9998';
+    }
   }
 });
 
