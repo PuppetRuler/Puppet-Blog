33fba7398c5165ead44a6f534afd6ac978aaecbf
PuppetRuler <ziyelixiang@gmail.com>
2024-10-02 14:44:58 +0800

    submit

--- docs/.vitepress/theme/components/ImgBlur.vue
+++ docs/.vitepress/theme/components/ImgBlur.vue
@@ -9,25 +9,52 @@
   image.src = originalURL;
 
   const load = async () => {
-    await new Promise((resolve) => (image.onload = resolve));
-    const canvas = document.createElement("canvas");
-    const context = canvas.getContext("2d");
-    const scale = 100 / Math.max(image.width, image.height);
-    canvas.width = Math.round(image.width * scale);
-    canvas.height = Math.round(image.height * scale);
-    context.drawImage(image, 0, 0, canvas.width, canvas.height);
-    const pixels = context.getImageData(0, 0, canvas.width, canvas.height);
-    const binaryThumbHash = ThumbHash.rgbaToThumbHash(
-      pixels.width,
-      pixels.height,
-      pixels.data
-    );
+    const img = new Image();
+    img.src = "../img/bg.jpg";
+    if (!img.complete) {
+      await new Promise((resolve) => (image.onload = resolve));
+      const canvas = document.createElement("canvas");
+      const context = canvas.getContext("2d");
+      const scale = 100 / Math.max(image.width, image.height);
+      canvas.width = Math.round(image.width * scale);
+      canvas.height = Math.round(image.height * scale);
+      context.drawImage(image, 0, 0, canvas.width, canvas.height);
+      const pixels = context.getImageData(0, 0, canvas.width, canvas.height);
+      const binaryThumbHash = ThumbHash.rgbaToThumbHash(
+        pixels.width,
+        pixels.height,
+        pixels.data
+      );
 
-    // ThumbHash to data URL
-    const placeholderURL = ThumbHash.thumbHashToDataURL(binaryThumbHash);
-
-    let vp = document.querySelector(".VPHome");
-    vp.style.setProperty("--bg-image", `url(${originalURL})`);
+      // ThumbHash to data URL
+      const placeholderURL = ThumbHash.thumbHashToDataURL(binaryThumbHash);
+      console.log(placeholderURL);
+      let style = document.createElement("style");
+      style.innerHTML = `
+        @media screen and (min-width:768px) {
+          .VPHome:before {
+            background-image: var(--bg-image, url(${placeholderURL}));
+            transition: background-image 0.6s;
+            background-size: cover;
+          }
+        }
+        `;
+      document.head.appendChild(style);
+      let vp = document.querySelector(".VPHome");
+      vp.style.setProperty("--bg-image", `url(${originalURL})`);
+    } else {
+      let style = document.createElement("style");
+      style.innerHTML = `
+      @media screen and (min-width:768px) {
+        .VPHome:before {
+          background-image: var(--bg-image, url(${originalURL}));
+          transition: background-image 0.6s;
+          background-size: cover;
+        }
+      }
+      `;
+      document.head.appendChild(style);
+    }
   };
   load();
 </script>
