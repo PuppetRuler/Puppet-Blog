c9c861f3c0f380db85e0032abc2cd6a81c9d4c95
PuppetRuler <ziyelixiang@gmail.com>
2024-10-20 21:56:08 +0800

    submit

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,16 @@
 import BlogTheme from '@sugarat/theme'
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import ImgBlur from './components/ImgBlur.vue'
+import Player from './components/Player.vue'
+import { h, render } from 'vue'
 
 // 自定义样式重载
 import './style.scss'
+
+// pinia
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 
 // 自定义主题色
 // import './user-theme.css'
@@ -10,6 +18,11 @@ import './style.scss'
 export default {
   ...BlogTheme,
   enhanceApp({ app }) {
-    app.component('ImgBlur', ImgBlur)
+    app.use(pinia);
+    app.component('ImgBlur', ImgBlur);
+    app.component('Player', Player);
+    const playerVNode = h(Player);
+    // 将 VNode 渲染到指定的 DOM 容器中
+    render(playerVNode, document.body);
   }
 }
